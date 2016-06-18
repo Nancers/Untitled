1e190226a29a5389b44ce4c9827e74437dd37d27
Nancy Cao <nancy.l.cao@gmail.com>
2016-06-17 22:50:57 -0700

    Added necessary textfiles for fillMap in PokemonMap

--- PokemonMap.java
+++ PokemonMap.java
@@ -9,23 +9,19 @@ import java.io.*;
 public class PokemonMap {
 
     public static HashMap<String, String[]> fillMap(String locF, String mapF) {
-
         //Part 1: Populate array of locations
         ArrayList<String> locations   = new ArrayList<String>();
         HashMap<String, String[]> map = new HashMap<String, String[]>();
 
         // Read through locations.txt to make an array of locations
         try {
-
             FileReader file       = new FileReader(locF);
             BufferedReader buffer = new BufferedReader(file);
             String nextLine       = buffer.readLine();
 
             while (nextLine != null) {
-
                 // Skip empty lines
                 if (nextLine.length() > 0) {
-
                     locations.add(nextLine);
                 }
 
@@ -35,30 +31,25 @@ public class PokemonMap {
             buffer.close();
         }
         catch(FileNotFoundException e) {
-
             System.out.println(locF + " not found in directory. \n");
         }
         catch(IOException e) {
-
             System.out.println("Error reading " + locF + "\n");
         }
 
         // Create mapping relationship of locations from map.txt
         try {
-
             FileReader file       = new FileReader(mapF);
             BufferedReader buffer = new BufferedReader(file);
             String nextLine       = buffer.readLine();
             StringBuffer locs     = new StringBuffer();
 
             while (nextLine != null) {
-
                 String[] indices = nextLine.split(" ");
                 String key       = locations.get(Integer.parseInt(indices[0]));
                 String[] vals    = new String[indices.length-1];
 
                 for (int i=1; i<indices.length; i++) {
-
                     vals[i-1] = locations.get(Integer.parseInt(indices[i]));
                 }
 
@@ -69,11 +60,9 @@ public class PokemonMap {
             buffer.close();
         }
         catch(FileNotFoundException e) {
-
             System.out.println(mapF + " not found in directory. \n");
         }
         catch(IOException e) {
-
             System.out.println("Error reading " + mapF + "\n");
         }
 
@@ -82,16 +71,13 @@ public class PokemonMap {
 
     // For testing purposes
     public static void main(String[] args) {
-
         HashMap<String, String[]> kanto = fillMap("locations.txt", "map.txt");
 
         // Printing out map
         for (String key : kanto.keySet()) {
-
             System.out.print(key + ": [");
 
             for (String loc : kanto.get(key)) {
-
                 System.out.print(loc + ", ");
             }
 
