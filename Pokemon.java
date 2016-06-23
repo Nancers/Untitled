88127ba6b3454ecfceaca5a1a6489b8baaa0ac2d
Nancy Cao <nancy.l.cao@gmail.com>
2016-06-23 16:11:59 -0700

    Added exp calculations for different level rates

--- Pokemon.java
+++ Pokemon.java
@@ -6,6 +6,7 @@ import java.io.*;
 import java.lang.*;
 
 public class Pokemon {
+
     public static final int MAX_IV          = 32;  // Max IV value
     public static final int MAX_EV          = 251; // Max EV value for any stat
     public static final int BASE_FRIENDSHIP = 70;  // Default friendship
@@ -44,17 +45,17 @@ public class Pokemon {
     // Constructs a wild Pokemon
     public Pokemon(int n, int minLvl, int maxLvl, Pokedex p) {
         id          = n;
-        nickname    = getName(n);
+        nickname    = p.getName(n);
         iv          = randNum(0, MAX_IV);
         ev          = 0;
         curLvl      = randNum(minLvl, maxLvl);
-        maxHP       = calcHP(getBaseHP(id));
+        maxHP       = calcHP(p.getBaseHP(id));
         curHP       = maxHP;
-        stats       = new String[];
+        stats       = new String[3];
         moves       = new Move[4];  // need a function to generate this
         heldItem    = "None";
         curExp      = 0;
-        totalExp    = 100;
+        totalExp    = calcExp(p.getLvlRate(id), curLvl);
         nature      = new Nature();
         attack      = calcStat(p.getBaseAttack(id), nature.getAttackMod());
         defense     = calcStat(p.getBaseDefense(id), nature.getDefenseMod());
@@ -74,11 +75,11 @@ public class Pokemon {
     }
 
     // Constructs a Pokemon in egg form
-    public Pokemon(int num, boolean egg, int steps) {
-        number = num;
+    public Pokemon(int num, boolean egg, int steps, Pokedex p) {
+        id = num;
         nickname = "Egg";
         curLvl = 1;
-        maxHP = genHP();
+        maxHP = calcHP(p.getBaseHP(id));
 
         isEgg = true;
         //hatchTime = genRandNum();
@@ -101,20 +102,98 @@ public class Pokemon {
     //--------------------------------------------------------------------------
 
     // Returns a random number [min, max]
-    public int randNum(int min, int max) {
+    private int randNum(int min, int max) {
         Random rand = new Random();
         return rand.nextInt(max + 1) + min;
     }
 
     // Returns rounded down stat calculation based on base stat
     // Input:   base - base stat
-    public int calcStat(int base, double nMod) {
-        return (int) (base * 2.0 + iv + (ev/4.0)) * curLvl/100.0 + 5 * nMod); 
+    private int calcStat(int base, double nMod) {
+        return (int) ((base * 2.0 + iv + (ev/4.0)) * curLvl/100.0 + 5 * nMod); 
     }
 
     // Returns rounded down HP calculation based on base HP
     // Input:   base - base HP
-    public int calcHP(int base) {
-        return (int) (base * 2.0 + iv + (ev/4.0)) * curLvl/100.0 + 10 + curLvl);
+    private int calcHP(int base) {
+        return (int) ((base * 2.0 + iv + (ev/4.0)) * curLvl/100.0 + 10 + curLvl);
     }
+
+    // Returns total experience needed for next level based on Pokemon's growth
+    // Input: rate - rate of growth
+    //        n    - current level
+    private int calcExp(LevelRate rate, int n) {
+        switch (rate) {
+            case ERRATIC:
+                return calcErraticExp(n);
+            case FAST:
+                return calcFastExp(n);
+            case MFAST:
+                return calcMfastExp(n);
+            case MSLOW:
+                return calcMslowExp(n);
+            case SLOW:
+                return calcSlowExp(n);
+            default:
+                return calcFluctuateExp(n);
+        }
+    }
+
+    // Returns experience needed for next lvl for ERRATIC Pokemon
+    // Input: n - current level
+    private int calcErraticExp(int n) {
+        if (n <= 50) {
+            return (int) (n * n * n * (100 - n) / 50.0);
+        }
+        else if (n <= 68) {
+            return (int) (n * n * n * (150 - n) / 100.0);
+        }
+        else if (n <= 98) {
+            return (int) (n * n * n * ((1911 - 10 * n) / 3.0) / 500.0);
+        }
+        else {
+            return (int) (n * n * n * (160 - n) / 100.0);
+        }
+    }
+
+    // Returns experience needed for next lvl for FAST Pokemon
+    // Input: n - current level
+    private int calcFastExp(int n) {
+        return (int) (4 * n * n * n / 5.0);
+    }
+
+    // Returns experience needed for next lvl for MFAST Pokemon
+    // Input: n - current level
+    private int calcMfastExp(int n) {
+        return n * n * n;
+    }
+
+    // Returns experience needed for next lvl for MSLOW Pokemon
+    // Input: n - current level
+    private int calcMslowExp(int n) {
+        return (int) (6 * n * n * n / 5.0 - 15 * n * n + 100 * n - 140);
+    }
+
+    // Returns experience needed for next lvl for SLOW Pokemon
+    // Input: n - current level
+    private int calcSlowExp(int n) {
+        return (int) (5 * n * n * n / 4.0);
+    }
+
+    // Returns experience needed for next lvl for FLUCTATE Pokemon
+    // Input: n - current level
+    private int calcFluctuateExp(int n) {
+        if (n <= 15) {
+            return (int) (n * n * n * ((n + 1) / 3.0 + 24) / 50.0);
+        }
+        else if (n <= 36) {
+            return (int) (n * n * n * ((n + 14) / 50.0));
+        }
+        else {
+            return (int) (n * n * n * (n / 2.0 + 32) / 50.0);
+        }
+    }
+
+    // Explanations on exp gain: 
+    // http://bulbapedia.bulbagarden.net/wiki/Experience
 }
