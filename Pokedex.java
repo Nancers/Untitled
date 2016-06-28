d254f56c81b186cee45b9b451590a7099215a773
jeffreyan11 <jeffreyan07@gmail.com>
2016-06-27 20:21:17 -0500

    Changed Pokedex to use a static inner class PokedexEntry.

--- Pokedex.java
+++ Pokedex.java
@@ -8,86 +8,93 @@ import java.util.*;
 import java.io.*;
 
 public class Pokedex {
-    private String[]                 name;          // Official name
-    private String[]                 altername;     // (e.g. Lizard Pokemon)
-    private String[]                 ability1;      // 1st known ability
-    private String[]                 ability2;      // 2nd known ability, if any
-    private String[]                 hiddenAbility; // Hidden ability
-    private Type[]                   type1;         // 1st type
-    private Type[]                   type2;         // 2nd type, if any
-    private int[]                    height;        // Height (inches)
-    private double[]                 weight;        // Weight (pounds)
-    private String[]                 bio;           // Pokedex biography
+    private static PokedexEntry pokedexDatabase[];
 
-    private int[]                    baseHP;        // base HP
-    private int[]                    baseAttack;    // base attack
-    private int[]                    baseDefense;   // base defense
-    private int[]                    baseSpAttack;  // base special attack
-    private int[]                    baseSpDefense; // base special defense
-    private int[]                    baseSpeed;     // base speed
-    private LevelRate[]              lvlRate;       // level rate
-    
-    private TreeMap<Integer, Move>[] movesToLearn;  // Level: move to learn
-    private Move[][]                 machineMoves;  // Moves to learn from HM/TM
-    private boolean[]                megaEvolve;    // Pokemon can mega-evolve
-    private boolean[]                evolves;       // Pokemon has evolution
-    private int                      evolveLvl;     // Level needed to evolve
-                                                    // -1 = requires a stone
-
-    private int[]                    catchRate;     // Catch rate at full HP
-
-    private int[]                    numSeen;       // # of times seen
-    private boolean[]                captured;      // Pokemon is captured
-
-    private String[]                 eggGroup1;     // 1st breed
-    private String[]                 eggGroup2;     // 2nd breed, if any
-    private int[]                    minHatchTime;  // Min steps to hatch
-    private int[]                    maxHatchTime;  // Max steps to hatch
-
-    private String[]                 pokedexArt;    // Art on Pokedex
-    private String                   frontSprite;   // Pokemon sprite file
-    private String                   backSprite;    // Pokemon sprite file
-    private String                   miniSprite;    // Sprite for inventory
-
-    // Pokemon database constructor. Reads in csv file and builds the Pokedex
-    public Pokedex() {
-        numSeen = new int[150];
-
+    // Pokemon database static initializer. Reads in csv file and builds the Pokedex
+    static {
+        pokedexDatabase = new PokedexEntry[150];
     }
 
-    //--------------------------------------------------------------------------
-    //----------------------------Getters/Setters-------------------------------
-    //--------------------------------------------------------------------------
-
-    public String getName(int n) {
-        return name[n-1];
+    public static PokedexEntry getPokedexEntry(int n) {
+        return pokedexDatabase[n-1];
     }
 
-    public int getBaseHP(int n) {
-        return baseHP[n-1];
-    }
+    public static class PokedexEntry {
+        private String                 name;          // Official name
+        private String                 altername;     // (e.g. Lizard Pokemon)
+        private String                 ability1;      // 1st known ability
+        private String                 ability2;      // 2nd known ability, if any
+        private String                 hiddenAbility; // Hidden ability
+        private Type                   type1;         // 1st type
+        private Type                   type2;         // 2nd type, if any
+        private int                    height;        // Height (inches)
+        private double                 weight;        // Weight (pounds)
+        private String                 bio;           // Pokedex biography
 
-    public int getBaseAttack(int n) {
-        return baseAttack[n-1];
-    }
+        private int                    baseHP;        // base HP
+        private int                    baseAttack;    // base attack
+        private int                    baseDefense;   // base defense
+        private int                    baseSpAttack;  // base special attack
+        private int                    baseSpDefense; // base special defense
+        private int                    baseSpeed;     // base speed
+        private LevelRate              lvlRate;       // level rate
+        
+        private TreeMap<Integer, Move> movesToLearn;  // Level: move to learn
+        private Move                   machineMoves;  // Moves to learn from HM/TM
+        private boolean                megaEvolve;    // Pokemon can mega-evolve
+        private boolean                evolves;       // Pokemon has evolution
+        private int                    evolveLvl;     // Level needed to evolve
+                                                        // -1 = requires a stone
 
-    public int getBaseDefense(int n) {
-        return baseDefense[n-1];
-    }
+        private int                    catchRate;     // Catch rate at full HP
 
-    public int getBaseSpAttack(int n) {
-        return baseSpAttack[n-1];
-    }
+        private int                    numSeen;       // # of times seen
+        private boolean                captured;      // Pokemon is captured
 
-    public int getBaseSpDefense(int n) {
-        return baseSpDefense[n-1];
-    }
+        private String                 eggGroup1;     // 1st breed
+        private String                 eggGroup2;     // 2nd breed, if any
+        private int                    minHatchTime;  // Min steps to hatch
+        private int                    maxHatchTime;  // Max steps to hatch
 
-    public int getBaseSpeed(int n) {
-        return baseSpeed[n-1];
-    }
+        private String                 pokedexArt;    // Art on Pokedex
+        private String                 frontSprite;   // Pokemon sprite file
+        private String                 backSprite;    // Pokemon sprite file
+        private String                 miniSprite;    // Sprite for inventory
 
-    public LevelRate getLvlRate(int n) {
-        return lvlRate[n-1];
+        //--------------------------------------------------------------------------
+        //----------------------------Getters/Setters-------------------------------
+        //--------------------------------------------------------------------------
+
+        public String getName(int n) {
+            return name;
+        }
+
+        public int getBaseHP(int n) {
+            return baseHP;
+        }
+
+        public int getBaseAttack(int n) {
+            return baseAttack;
+        }
+
+        public int getBaseDefense(int n) {
+            return baseDefense;
+        }
+
+        public int getBaseSpAttack(int n) {
+            return baseSpAttack;
+        }
+
+        public int getBaseSpDefense(int n) {
+            return baseSpDefense;
+        }
+
+        public int getBaseSpeed(int n) {
+            return baseSpeed;
+        }
+
+        public LevelRate getLvlRate(int n) {
+            return lvlRate;
+        }
     }
 }
--- Pokemon.java
+++ Pokemon.java
@@ -6,45 +6,47 @@ import java.io.*;
 import java.lang.*;
 
 public class Pokemon {
-
     public static final int MAX_IV          = 32;  // Max IV value
     public static final int MAX_EV          = 251; // Max EV value for any stat
     public static final int BASE_FRIENDSHIP = 70;  // Default friendship
     public static final int MAX_FRIENDSHIP  = 255; // Max friendship
     public static final int NUM_NATURE      = 25;  // number of possible natures
 
-    private int      id;         // ID # on the regional Pokedex; starts at 1
-    private String   nickname;   // Name given by trainer, if any
-    private int      maxHP;      // Max health 
-    private int      curHP;      // Current health
-    private String[] stats;      // Stats inflicted (can you have multiple?)
-    private Move[]   moves;      // Up to 4 moves
-    private String   heldItem;   // Need a parent/abstract class of items?
-    private int      curLvl;     // Current level
-    private int      curExp;     // Current exp gained for current level
-    private int      totalExp;   // Exp needed for next level
+    private Pokedex.PokedexEntry pokemonData; // The static pokemon data from the Pokedex
 
-    private int      attack;     // Current attack value
-    private int      defense;    // Current defense
-    private int      spAttack;   // Current special attack
-    private int      spDefense;  // Current special defense
-    private int      speed;      // Current speed
-    private int      friendship; // Current friendship
-    private String   gender;     // Male or female
-    private Nature   nature;     // Used to calculate stat growth
-    private String   character;  // Used to calculate stat growth
-    private int      iv;         // Individual value
-    private Effort   ev;         // Effort values
+    private int          id;         // ID # on the regional Pokedex; starts at 1
+    private String       nickname;   // Name given by trainer, if any
+    private int          maxHP;      // Max health 
+    private int          curHP;      // Current health
+    private String[]     stats;      // Stats inflicted (can you have multiple?)
+    private Move[]       moves;      // Up to 4 moves
+    private String       heldItem;   // Need a parent/abstract class of items?
+    private int          curLvl;     // Current level
+    private int          curExp;     // Current exp gained for current level
+    private int          totalExp;   // Exp needed for next level
 
-    private boolean  isEgg;      // If Pokemon is in egg form
-    private int      hatchTime;  // Steps left to hatch
+    private int          attack;     // Current attack value
+    private int          defense;    // Current defense
+    private int          spAttack;   // Current special attack
+    private int          spDefense;  // Current special defense
+    private int          speed;      // Current speed
+    private int          friendship; // Current friendship
+    private String       gender;     // Male or female
+    private Nature       nature;     // Used to calculate stat growth
+    private String       character;  // Used to calculate stat growth
+    private int          iv;         // Individual value
+    private Effort       ev;         // Effort values
+
+    private boolean      isEgg;     // If Pokemon is in egg form
+    private int          hatchTime; // Steps left to hatch
 
     //--------------------------------------------------------------------------
     //------------------------------Constructors--------------------------------
     //--------------------------------------------------------------------------
 
     // Constructs a wild Pokemon
-    public Pokemon(int i, int minLvl, int maxLvl, Pokedex p, Naturedex n) {
+    public Pokemon(int i, int minLvl, int maxLvl, Pokedex.PokedexEntry p, Naturedex n) {
+        pokemonData = p;
         id          = i;
         nickname    = p.getName(id);
         iv          = randNum(0, MAX_IV);
