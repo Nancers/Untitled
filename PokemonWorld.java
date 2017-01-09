283fdf6b719a2afee8512fbbf206788d4b3471e1
Nancy Cao <nancy.l.cao@gmail.com>
2017-01-09 10:36:52 -0800

    Begin separating the world from the game

---
+++ PokemonWorld.java
@@ -0,0 +1,33 @@
+/* An instance of this represents the current area the player is at.
+ */
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class PokemonWorld {
+
+    private String worldURL;
+    private BufferedImage worldImage;
+    private int width;
+    private int height;
+    private int[][] areaMatrix;
+
+    // Do we write the matrix beforehand?? Do we just the current area and
+    // matrix together idk
+    public PokemonWorld(String url, int w, int h) {
+
+        worldURL = url;
+
+        // Create buffered image of current area
+        try {
+            mapImage = ImageIO.read(new File("pics" + File.separator + "pallet_town.png"));
+        }
+        catch (IOException e) {}
+
+        width = w;
+        height = h;
+
+        areaMatrix = new int[w][h];
+    }
+}
